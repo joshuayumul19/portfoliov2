ea7a77911e1fd94c2bcccb2c83002af57d2361dd
joshy <108915592+joshuayumul19@users.noreply.github.com>
2024-11-19 21:46:20 +0800

    Updating the resume link

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,18 +1,21 @@
 <template>
-	<nav class="bg-white sticky top-0 z-40 py-10" :class="{ shadow: isScrolled }">
+	<nav
+		class="bg-white sticky top-0 z-40 py-10"
+		:class="{
+			shadow: isScrolled,
+		}"
+	>
 		<div class="flex flex-wrap items-center justify-center md:justify-between mx-auto max-w-[65rem] px-4">
 			<a href="#" class="flex items-center">
 				<span class="self-center text-2xl font-bold whitespace-nowrap dark:text-white">Joshua</span>
 			</a>
 			<div class="hidden w-full md:block md:w-auto">
-				<ul
-					class="flex flex-col p-4 md:p-0 mt-4 text-md border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-5 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
-				>
+				<ul class="flex flex-col p-4 md:p-0 mt-4 text-md border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-5 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
 					<li v-for="list in navLists">
 						<a :href="list.href" class="block py-1 px-5 bg-white hover:bg-slate-200 rounded-md">{{ list.label }}</a>
 					</li>
 					<li>
-						<a href="https://drive.google.com/file/d/1rjBmX5kCSjZDI8f5PVHIusQfvrhP9n7z/view?usp=sharing" target="_blank" class="block py-1 px-5 bg-white hover:bg-slate-200 rounded-md">
+						<a href="https://drive.google.com/file/d/1zAkI2D86m67whCCMpvSkMFUo-WmNhHHr/view?usp=sharing" target="_blank" class="block py-1 px-5 bg-white hover:bg-slate-200 rounded-md">
 							Resume
 							<font-awesome-icon icon="fa-solid fa-arrow-up-right-from-square" />
 						</a>
